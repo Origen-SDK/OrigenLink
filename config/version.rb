561deb4c64b6988d053c04bc79a1deb561c7abe7
Derouen Paul-B04525 <B04525@freescale.com>
2016-07-06 13:57:17 -0500

    Updated app version and history

--- config/version.rb
+++ config/version.rb
@@ -1,8 +1,8 @@
 module OrigenLink
   MAJOR = 0
-  MINOR = 1
-  BUGFIX = 2
-  DEV = nil
+  MINOR = 2
+  BUGFIX = 0
+  DEV = 0
 
   VERSION = [MAJOR, MINOR, BUGFIX].join(".") + (DEV ? ".pre#{DEV}" : '')
 end
